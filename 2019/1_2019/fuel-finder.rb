bfac080b42aa7d9b474213f7b0e012ce65bd292c
Brenna Flood <bren@brennx0r.com>
2019-12-01 17:46:57 -0800

    Solution for part 2

--- 2019/1_2019/fuel-finder.rb
+++ 2019/1_2019/fuel-finder.rb
@@ -1,14 +1,37 @@
+
+@module_fuel = []
+@fuel_agg = [] 
+
 def input(file) 
     @file_arr = File.readlines(file)
 end
 
+def fuel_of_fuel 
+    @module_fuel.each do |num|
+        fuel_of_fuel = (num.to_i / 3).round - 2
+
+        if fuel_of_fuel < 0
+            fuel_of_fuel == 0
+            next
+        end
+
+        @module_fuel.push(fuel_of_fuel)
+        @fuel_agg.push(fuel_of_fuel)
+        puts @module_fuel.to_s
+        puts @module_agg.to_s
+    end
+end
+
+
+
 def required_fuel(file_arr)
-    module_fuel = []
     file_arr.each do |mass|
         required_fuel = (mass.to_i / 3).round - 2
-        module_fuel.push(required_fuel)
+        @module_fuel.push(required_fuel)
     end
-    puts module_fuel.inject(0){|sum,x| sum + x }
+    fuel_sum = @module_fuel.inject(0){|sum,x| sum + x }
+    @fuel_agg.push(fuel_sum)
+    fuel_of_fuel
 end
 
 def run_tests
@@ -17,3 +40,5 @@ end
 
 required_fuel(input('input.txt'))
 #run_tests
+
+puts "Total in agg: "+@fuel_agg.inject(0){|sum,x| sum + x }.to_s

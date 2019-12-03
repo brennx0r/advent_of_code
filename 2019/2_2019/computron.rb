3be637aae0af85c156a26a8be7a8030cb8564feb
Brenna Flood <bren@brennx0r.com>
2019-12-02 21:21:47 -0800

    An attempt was made for part 2 - doesn't yet work correctly

--- 2019/2_2019/computron.rb
+++ 2019/2_2019/computron.rb
@@ -1,32 +1,50 @@
-@instruction_array = []
-@instruction_array_clone = []
+@noun = 0 
+@verb = 99
+
+def reset_arrays
+    @instruction_array = []
+    @instruction_array_clone = []
+end
 
 def input(file) 
     @result = File.readlines(file)
 end
 
-def pre_run_replacements
+def pre_run_replacement_noun
+    puts "noun: "+@noun.to_s
     @instruction_array.delete_at(1)
-    @instruction_array.insert(1, "12")
+    @instruction_array.insert(1, @noun.to_s)
+    @noun+= 1
+end
+
+def pre_run_replacement_verb
+    puts "verb: "+@verb.to_s
     @instruction_array.delete_at(2)
-    @instruction_array.insert(2, "2")
+    @instruction_array.insert(2, @verb.to_s)
+    @verb -= 1
 end
 
 def convert_to_array(input)
+    reset_arrays
     input.each do |line|
         @instruction_array = line.split(',')
     end
     @instruction_array_clone = @instruction_array.clone
-    pre_run_replacements
+    pre_run_replacement_noun
+    pre_run_replacement_verb
     read_index_0
 end
 
 def exit_check
-    if @instruction_array_clone[0] == "99"
-        puts "Reached 99 - exiting program with result at index 0: "+@instruction_array.to_s
+    if @instruction_array_clone[0] == "99" and @instruction_array[1].to_s = "19690720"
+        puts "Reached 99 - exiting program run. Result number is "+@instruction_array[0].to_s
         exit
-    end
+    else
+        run
+        puts "Beginning another program run since we didn't get result '19690720'"  
     
+    end
+
     # if @instruction_array.count > 50
     #     puts "somethings wrong - exiting"
     #     exit

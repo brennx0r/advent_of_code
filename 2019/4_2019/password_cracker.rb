b21a11d4965d44e3023fd8a5fea4861d6734b1d6
Brenna Flood <brenna@splice.com>
2019-12-04 17:44:05 -0800

    The WIP - it continues

--- 2019/4_2019/password_cracker.rb
+++ 2019/4_2019/password_cracker.rb
@@ -3,23 +3,42 @@
 
 def check_length
     puts "Checking the length!"
-    if @number.length > 6 or @number.join = @end_number
+    if @array.length > 6 or @array.join == @end_number
+        exit
+    end
+end
+
+def adjacent_digit_match
+    puts "Running adjacent match check!"
+    if @string.match('.*(00|11|22|33|44|55|66|77|88|99).*')
+        puts "Match!"
+        puts "Resulting number: "+@string
         exit
     end
 end
 
 def run_checks
     puts "Running the checks!"
+    check_length
+    adjacent_digit_match
 end
 
 def iterate
     puts "Iterating the number!"
+    number = @string.to_i
+    number += 1
+    @string = number.to_s
+    crack
 end
 
 
 def convert_to_array
     puts "Converting input to an array!"
-    @array = @start_number.chars
+    if @array == []
+        @array = @start_number.chars
+    else
+        @array = @string.chars
+    end
     puts "Array created: "+@array.to_s
 end
 
@@ -34,8 +53,8 @@ end
 def crack
     convert_to_array
     convert_to_string
-    iterate
     run_checks
+    iterate
 end
 
 puts "Let's crack some passwords!\n\nWhat is the beginning range for the crack?"

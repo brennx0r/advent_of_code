3a998e280e4d9f5a4b8fdd638f35934163af921a
Brenna Flood <bren@brennx0r.com>
2020-12-01 13:24:25 -0800

    Day 1: Solution for Part 2

--- 2020/1/1.rb
+++ 2020/1/1.rb
@@ -10,35 +10,45 @@ end
 
 def add_hop_values(array)
   array_length = array.length
-  times_headroom = array_length * array_length
+  times_headroom = array_length * array_length * array_length
   array_setting = 0
   array_setting_jump = 1
+  array_setting_jump_two = 2
 
   times_headroom.times {
-    addition_result = array[array_setting] + array[array_setting_jump]
+    puts "Combination: "+array_setting.to_s+" "+array_setting_jump.to_s+" "+array_setting_jump_two.to_s
+    addition_result = array[array_setting] + array[array_setting_jump] + array[array_setting_jump_two]
 
     puts "Addition result is: " +addition_result.to_s
 
     if addition_result == 2020
       puts "we have 2020"
-      multiply_nums(array[array_setting], array[array_setting_jump])
+      multiply_nums(array[array_setting], array[array_setting_jump], array[array_setting_jump_two])
       break
     else
       puts "we don't have 2020"
-      array_setting_jump += 1
+      array_setting_jump_two += 1
     end
 
-    if array_setting_jump == array_length
+    if array_setting_jump_two == array_length 
+      array_setting_jump_two = array_setting_jump + 2
+      array_setting_jump = array_setting_jump + 1
+    end
+
+    if array_setting_jump == array_length - 1
       array_setting += 1
       array_setting_jump = array_setting + 1
+      array_setting_jump_two = array_setting + 2
     end
+
   }
 end
 
-def multiply_nums(num1, num2)
+def multiply_nums(num1, num2, num3)
   puts "num1 is: " + num1.to_s
   puts "num2 is: " + num2.to_s
-  @num_result = num1 * num2
+  puts "num3 is: " + num3.to_s
+  @num_result = num1 * num2 * num3
 end
   
 read_to_array('test_data_1.txt')

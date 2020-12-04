028837b3cec15d6ea41a9aff37acbe10a33ad454
Brenna Flood <bren@brennx0r.com>
2020-12-04 11:38:42 -0800

    New: Script and sample file to seed day solutions

---
+++ 2020/genesis.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+if [ $1 == "--help" ]; then
+    echo "NAME"
+    echo "       genesis.sh - creates 'seed' folders and files to assist in working through the days in Advent of Code."
+    echo ""
+    echo ""
+    echo "SYNOPSIS"
+    echo '      genesis.sh [$1]'
+    echo ""
+    echo '       $1 - a number expressing the day you would like to generate. This will create a numbered folder, with seed ruby and txt files underneath.'
+    exit 0
+fi
+
+day=$1
+
+
+mkdir ${day}
+touch ${day}/test_data.txt
+touch ${day}/test_data_sample.txt
+cp sample.rb ${day}/${day}.rb

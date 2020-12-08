ec5c35fcc09eee63be8185eef42bc79017916ab6
Brenna Flood <bren@brennx0r.com>
2020-12-07 16:07:40 -0800

    Clean up a little

--- 2020/5/5.rb
+++ 2020/5/5.rb
@@ -12,20 +12,13 @@ def read_to_array(file)
         
         row = traverse_front_to_back(@ftb_array)
         seat_line = traverse_left_to_right(@ltr_array)
-
-        puts row
-        puts seat_line
-
         result = row * 8 + seat_line
-        
-
         seat_id_eval(result)
         
     end
 end
 
 def traverse_front_to_back(ftb_array)
-    puts "Starting Array: "+ftb_array.to_s
 
     # let's build a row array
     @row_array = [] 
@@ -38,12 +31,7 @@ def traverse_front_to_back(ftb_array)
 
     while ftb_array.length != 0
 
-        puts "Current array: "+@row_array.to_s
-        puts "Current Instructions: "+ftb_array.to_s
-        puts "Current length: "+@length.to_s
-        
         if ftb_array[0] == "F"
-        puts "f"
             @number_start_value = @row_array[0]
             @number_start = @row_array.index(@number_start_value)
             @range = @row_array.length/2
@@ -52,20 +40,14 @@ def traverse_front_to_back(ftb_array)
         end 
 
         if ftb_array[0] == "B"
-        puts "b"
             @range = @row_array.length/2
             @number_start_value = @row_array[@range]
             @number_start = @number_start = @row_array.index(@number_start_value)
             @number_end_value = @row_array.last
             @number_end = @row_array.index(@number_end_value)
-
-            puts @length
         end
-
-        puts @number_start_value.to_s
-        puts @number_end_value.to_s
-        @row_array = @row_array[@number_start..@number_end]
-        puts @row_array.to_s  
+        
+        @row_array = @row_array[@number_start..@number_end] 
         ftb_array.delete_at(0)
     end
       
@@ -87,12 +69,7 @@ def traverse_left_to_right(ltr_array)
 
     while ltr_array.length != 0
 
-        puts "Current array: "+@row_array.to_s
-        puts "Current Instructions: "+ltr_array.to_s
-        puts "Current length: "+@length.to_s
-        
         if ltr_array[0] == "L"
-        puts "l"
             @number_start_value = @row_array[0]
             @number_start = @row_array.index(@number_start_value)
             @range = @row_array.length/2
@@ -101,20 +78,14 @@ def traverse_left_to_right(ltr_array)
         end 
 
         if ltr_array[0] == "R"
-        puts "r"
             @range = @row_array.length/2
             @number_start_value = @row_array[@range]
             @number_start = @number_start = @row_array.index(@number_start_value)
             @number_end_value = @row_array.last
             @number_end = @row_array.index(@number_end_value)
-
-            puts @length
         end
 
-        puts @number_start_value.to_s
-        puts @number_end_value.to_s
-        @row_array = @row_array[@number_start..@number_end]
-        puts @row_array.to_s  
+        @row_array = @row_array[@number_start..@number_end] 
         ltr_array.delete_at(0)
     end
       
@@ -127,7 +98,6 @@ def seat_id_eval(seat_id)
     #let's sort em in this array
     @seat_id_array.push(seat_id)
     @seat_id_array.sort!
-    puts @seat_id_array.to_s
     puts "HIGHEST SEAT ID: "+@seat_id_array.last.to_s
 end
 

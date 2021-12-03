58a4977e47e8781d72c788df2b0306b4c818e32f
Brenna Flood <bren@brennx0r.com>
2021-12-03 15:38:05 -0800

    Formatting updates

--- 2021/1/1.rb
+++ 2021/1/1.rb
@@ -1,4 +1,3 @@
-
 @increases = 0
 @decreases = 0
 @sames = 0
@@ -10,11 +9,9 @@ def read_to_array(file)
     read_lines.each do |line|
         @array << line.to_i
     end
-
 end
 
 def  eval_values
-
     if @array[0] < @array[1]
         @increases += 1
     elsif @array[0] == @array[1]
@@ -23,21 +20,15 @@ def  eval_values
         @decreases += 1
     else
     end
-
     remove_first_element
-
 end
 
 def remove_first_element
-
     @array.delete_at(0)
-
     eval_continue
-
 end
 
 def eval_continue
-
     if @array.count > 1
         eval_values   
     end
@@ -45,7 +36,4 @@ end
 
 read_to_array('test_data.txt')
 eval_values
-
-
-
 puts @increases.to_s

f76a2803b1b57f788fd8ed3f8851792d2a0e379a
Brenna Flood <bren@brennx0r.com>
2021-12-04 15:08:42 -0800

    Solution: Day 3, part 1

--- 2021/3/3.rb
+++ 2021/3/3.rb
@@ -1,10 +1,86 @@
+@row_count = 0
+@gamma = []
+@epsilon = []
+class Binary
+    def self.to_decimal(binary)
+        raise ArgumentError if binary.match?(/[^01]/)
+
+        binary.reverse.chars.map.with_index do |digit, index|
+        digit.to_i * 2**index
+        end.sum
+    end
+end
+  
+
+  
 def read_to_array(file)
     read_lines = File.readlines(file)
     @array = []
 
     read_lines.each do |line|
-        @array << line.to_s
+        scrubbed_line = line.gsub(/\n/, "")
+        @array << scrubbed_line.split("")
+        @row_count += 1
+
     end
+
+    evaluate_gamma
 end
 
-read_to_array('test_data_sample.txt')
+def evaluate_gamma
+    length = @array[0].count
+    
+    length.times do
+    
+        tmp_array = []
+        n = 0
+        ones = 0
+        zeroes = 0
+        @row_count.times do
+
+            if @array[n][0] == "1"
+                ones += 1
+                @array[n].shift()
+            elsif
+                @array[n][0] == "0"
+                zeroes += 1
+                @array[n].shift()
+            else
+            end
+
+            puts ones
+            puts zeroes
+            n+= 1
+        end
+
+        if ones > zeroes 
+            @gamma << 1
+            @epsilon << 0
+        else
+            @gamma << 0
+            @epsilon << 1
+        end
+    end
+
+    form_final_gamma_binary
+end
+
+def form_final_gamma_binary
+    gamma_binary = @gamma.join("")
+    @gamma_decimal = Binary.to_decimal(gamma_binary)
+    form_epsilon
+end
+
+def form_epsilon 
+    epsilon_binary = @epsilon.join("")
+    @epsilon_decimal = Binary.to_decimal(epsilon_binary)
+end
+
+
+read_to_array('test_data.txt')
+
+result = @gamma_decimal * @epsilon_decimal
+puts "Result: "+result.to_s
+
+
+

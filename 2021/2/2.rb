ed39ef125a9e264c1ce07a05272a4737f2603b34
Brenna Flood <bren@brennx0r.com>
2021-12-03 22:40:07 -0800

    Day 2, part 1 solution

--- 2021/2/2.rb
+++ 2021/2/2.rb
@@ -1,3 +1,6 @@
+@forward = 0
+@depth = 0
+
 def read_to_array(file)
     read_lines = File.readlines(file)
     @array = []
@@ -7,4 +10,40 @@ def read_to_array(file)
     end
 end
 
-read_to_array('test_data_sample.txt')
+def process_lines
+
+    num_elements = @array.count
+
+    num_elements.times do
+        if @array[0].include?"forward"
+            value = @array[0].gsub(/forward /, '')
+            @forward += value.to_i
+        end
+
+        if @array[0].include?"down"
+            value = @array[0].gsub(/down /, '')
+            @depth += value.to_i
+        end
+        
+        if @array[0].include?"up"
+            value = @array[0].gsub(/up /, '')
+            @depth -= value.to_i
+        end
+        @array.shift    
+    end
+
+    multiply_values
+
+end
+
+def multiply_values
+
+    @result = @depth * @forward
+
+end
+
+read_to_array('test_data.txt')
+process_lines
+
+
+puts @result.to_s

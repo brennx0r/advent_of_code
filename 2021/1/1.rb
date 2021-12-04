aa72bcf22359562df432039701266352c2711395
Brenna Flood <bren@brennx0r.com>
2021-12-03 16:12:37 -0800

    Day 1, part 2 solution

--- 2021/1/1.rb
+++ 2021/1/1.rb
@@ -1,6 +1,7 @@
 @increases = 0
 @decreases = 0
 @sames = 0
+@sum_array = []
 
 def read_to_array(file)
     read_lines = File.readlines(file)
@@ -11,12 +12,39 @@ def read_to_array(file)
     end
 end
 
+def add_elements
+
+    array_count = @array.count
+    n = 0
+
+    array_count.times do
+
+        if array_count - n == 2
+            break
+        elsif array_count - n == 1
+            break
+        else
+        
+            sum = @array[n] + @array[n+1] + @array[n+2]
+            @sum_array << sum
+            puts @sum_array.to_s
+        
+            n += 1
+
+        end
+    end
+
+    eval_values
+
+end
+
+
 def  eval_values
-    if @array[0] < @array[1]
+    if @sum_array[0] < @sum_array[1]
         @increases += 1
-    elsif @array[0] == @array[1]
+    elsif @sum_array[0] == @sum_array[1]
         @sames += 1
-    elsif @array[0] > @array[1]
+    elsif @sum_array[0] > @sum_array[1]
         @decreases += 1
     else
     end
@@ -24,16 +52,17 @@ def  eval_values
 end
 
 def remove_first_element
-    @array.delete_at(0)
+    @sum_array.delete_at(0)
     eval_continue
 end
 
 def eval_continue
-    if @array.count > 1
+    if @sum_array.count > 1
         eval_values   
     end
 end
 
 read_to_array('test_data.txt')
-eval_values
+add_elements
+
 puts @increases.to_s

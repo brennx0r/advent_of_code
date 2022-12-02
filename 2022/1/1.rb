980bbaaabe1809acfad42d0d388fd532eb10f86f
Brenna Flood <bren@brennx0r.com>
2022-12-01 17:43:48 -0800

    Solution for Day 1, Part 1, using sample data.

---
+++ 2022/1/1.rb
@@ -0,0 +1,20 @@
+def read_to_array(file)
+    read_lines = File.readlines(file)
+    @array = []
+    @calorie_totals = []
+
+    read_lines.each do |line|
+        if line.to_i != 0
+            @array << line.to_i
+        else
+            @calorie_totals << @array.sum
+            @array = []
+        end    
+    end
+end
+
+
+
+read_to_array('test_data_sample.txt')
+puts @calorie_totals.max
+

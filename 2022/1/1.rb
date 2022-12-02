bb7c9b1533f673f74b31116b59c6f0f82561d62f
Brenna Flood <bren@brennx0r.com>
2022-12-01 18:01:17 -0800

    Solved Day 1, Part 2 using the sample set

--- 2022/1/1.rb
+++ 2022/1/1.rb
@@ -11,10 +11,16 @@ def read_to_array(file)
             @array = []
         end    
     end
+    @calorie_totals << @array.sum
 end
 
-
+def calc_top_3
+    sorted_array = @calorie_totals.sort.reverse!
+    puts sorted_array
+    sum = sorted_array[0]+sorted_array[1] + sorted_array[2]
+    puts sum
+end
 
 read_to_array('test_data_sample.txt')
-puts @calorie_totals.max
+calc_top_3
 

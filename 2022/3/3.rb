a1b53cdf3e499f1e1a2c6cdf47a5f1a789ec3ef6
Brenna Flood <bren@brennx0r.com>
2022-12-03 13:33:17 -0800

    Solution for Day 3, part 2 using the sample data set.

--- 2022/3/3.rb
+++ 2022/3/3.rb
@@ -1,3 +1,5 @@
+@array = []
+@trio = []
 @value = 0
 
 def make_alphabet_array
@@ -12,37 +14,34 @@ def read_to_array(file)
     read_lines = File.readlines(file)
     
     read_lines.each do |line|
-        array = []
-        split_array = [] 
-        split_array_1 = []
-        split_array_2 = []
+        @array << line.chomp         
+    end
+end
 
-        #split the string into an array w/ each element as separate characters
-        array << line.chomp
-        split_array = array[0].split("")
-
-        # split the line in two equal length arrays with a single 
-        # letter in each array
-        half_length = split_array.length / 2
+def evaluate_trio
+    length = @array.length / 3
+    
+    length.times {
+        @trio = []
+            3.times {
+                @trio << @array[0]
+                @array.shift()
+            }
+        array1 = @trio[0].split("")
+        array2 = @trio[1].split("")
+        array3 = @trio[2].split("")
         
-        split_array_1 << split_array.slice(0,half_length)
-        split_array_2 << split_array.slice(half_length, split_array.length)
-        split_array_1 = split_array_1[0]
-        split_array_2 = split_array_2[0]
+        common_letter = array1 & array2 & array3
 
-        # intersect the arrays and return the common letter
-        common_letter = split_array_1 & split_array_2
-
-        # Evaluate the letter (upper vs lower case)     
         if !common_letter.to_s.match(/\p{Upper}/) == true
             @value += @alphabet_array.index(common_letter[0]) + 1
         elsif !common_letter.to_s.match(/\p{Lower}/) == true
             @value += @alphabet_array.index(common_letter[0].downcase) + 27
-        end       
-    end
+        end 
+    }
 end
 
 make_alphabet_array
 read_to_array('test_data_sample.txt')
-# puts @split_array.to_s
+evaluate_trio
 puts @value

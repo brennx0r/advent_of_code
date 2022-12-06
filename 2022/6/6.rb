4d7cdefda934d3b36a5c91b9a48b2ad2b074bd5c
Brenna Flood <bren@brennx0r.com>
2022-12-05 22:14:29 -0800

    Solution using the sample data set, part 1

---
+++ 2022/6/6.rb
@@ -0,0 +1,43 @@
+@count = 0
+@saved_stream = []
+
+def read_to_array(file)
+    read_lines = File.readlines(file)
+
+    read_lines.each do |line|
+        @array = line.chomp.split("")
+    end
+end
+
+def process_stream
+    length = @array.length.to_i
+        puts @array[0].to_s
+        
+    4.times {
+        @saved_stream << @array[0]
+        @array.shift
+        @count += 1
+    }
+    
+    
+
+    10.times {
+        unique_arr =[]
+        @saved_stream << @array[0]
+        unique_arr = @saved_stream.uniq
+        puts @saved_stream.to_s
+        puts unique_arr.to_s
+        if unique_arr.length == 5
+            puts "FOUND IT: "+@count.to_s
+        else
+            @count += 1
+            @array.shift
+            @saved_stream.shift
+        end
+    }
+
+    
+end
+
+read_to_array('test_data_sample.txt')
+process_stream

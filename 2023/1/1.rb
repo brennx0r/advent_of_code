98ad6297f5909a8dd8efeb7ba3e28f208ecfcb12
Brenna Flood <brennx0r@selune.local>
2023-12-01 16:56:11 -0800

    Solution for Day 1, Part 2

--- 2023/1/1.rb
+++ 2023/1/1.rb
@@ -9,13 +9,45 @@ def read_line(file)
 
     read_lines.each do |line|
         
-        line_array = line.gsub(/[^0-9]/, '').split("")
-        puts @line_array.to_s
+        # here we go with some ugly interpolation, whee
+
+        corner_cases = {
+            "twone" => "21",
+            "oneight" => "18",
+            "eightwo" => "82",
+            "eighthree" => "83",
+            "threeight" => "38",
+            "fiveight" => "58",
+            "sevenine" => "79",
+            "nineight" => "98"
+        }
+
+        num_values = {
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+            
+
+        }
+
+        puts line 
+       
+        line_corner_cases = line.gsub(/twone|oneight|eightwo|eighthree|threeight|fiveight|sevenine|nineight/, corner_cases)
+        new_str = line_corner_cases.gsub(/one|two|three|four|five|six|seven|eight|nine/, num_values)
+        line_array = new_str.gsub(/[^0-9]/, '').split("")
+        puts line_array.to_s
 
         number = line_array.first + line_array.last
 
         calib_num_arr << number.to_i
 
+        puts calib_num_arr.to_s
         puts calib_num_arr.sum
 
     end
